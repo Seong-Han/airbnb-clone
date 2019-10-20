edc005e962e4b94132bf05915598d5162ad1f282
sunghan <incastle@sunghanui-MacBook-Pro.local>
2019-10-20 23:45:37 +0900

    5.0

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from core import models as core_model
+
+
+class Review(core_model.TimeStampedModel):
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.review} - {self.room}"
+
